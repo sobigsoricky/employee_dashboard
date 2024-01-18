d05cc74ece898cbdda8c1174d10127ed2a968ad7
adityajain-web <pranay.mishra.del@gmail.com>
2024-01-18 14:11:40 +0530

    empty

--- src/utils/generateOTP.js
+++
@@ -1,3 +0,0 @@
-export default function () {
-    return Math.floor(1000 + Math.random() * 9000).toString();
-  }
--- src/utils/mailHelper/index.js
+++
@@ -1,5 +0,0 @@
-import { sendMail } from "./sendMail";
-import { TEMPLATE_VERIFY_YOUR_EMAIL } from "./emailTemplates";
-import generateOTP from "../generateOTP";
-
-export { sendMail, TEMPLATE_VERIFY_YOUR_EMAIL, generateOTP };
